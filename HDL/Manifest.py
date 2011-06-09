b1fec995632070f01d2cb131762018fbf00789ad
twlostow <twlostow@665b4545-5c6b-4c24-801b-41150b02b44b>
2011-06-09 07:47:04 +0000

    added hdlmake manifest

--- HDL/Manifest.py
+++ HDL/Manifest.py
@@ -1,3 +1,11 @@
-modules = {
-"local" : [ "VME64e_ActHDL_src", "NOIP_cores" ]
-}
+files = [
+"VME64e_ActHDL_src/VME_bus.vhd",
+"VME64e_ActHDL_src/VME_CR_pack.vhd",
+"VME64e_ActHDL_src/wb_dma.vhd",
+"VME64e_ActHDL_src/VME_CSR_pack.vhd",
+"VME64e_ActHDL_src/VME_pack.vhd",
+"VME64e_ActHDL_src/SharedComps.vhd",
+"NOIP_cores/DpRam/DpBlockRam.vhd",
+"NOIP_cores/DpRam/TrueDpBlockRam.vhd",
+"NOIP_cores/fifo/common_components.vhd",
+"VFC_dev/VME64xCore_NoIpTop.vhd" ];
